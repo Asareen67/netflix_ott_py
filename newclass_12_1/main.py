1c04bc278633569580107c679377f6a731c8c0f2
Asareen67 <64062691+Asareen67@users.noreply.github.com>
2026-01-19 12:23:14 +0530

    Basic Topics of Python
    Here i have made some basic programs of python like data types varaiable and functions

---
+++ newclass_12_1/main.py
@@ -0,0 +1,29 @@
+a = 9  
+
+# opertors
+
+# Arthmetic Operators -> + , - , * , / , // , ** , %
+
+# print(9%2)
+# Comparison Operators -> compare the values
+# == , != , < , > , <= , >=
+a = 15
+b = 12
+if (a<b):
+    print("Yes", a ,"is greater than ",b)
+else:
+    print("No 9 is not less than 5")
+
+
+# identical -> compare the variables
+# is is not 
+a = [8,9]
+b = [8,9]
+print(a is b)
+
+# Logical Operators -> compare the conditions
+# and , not , or 
+# print(6==6 and 5>3)
+# print(6!=6 or 9==9)
+print(not (6==6 or 7<2))
+
